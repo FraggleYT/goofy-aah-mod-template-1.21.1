ebce4c0d7cf5a5a78b611299b5d440f327e44edd
Fraggle <97068671+FraggleYT@users.noreply.github.com>
2024-09-27 20:47:03 +0200

    Initial commit

---
+++ src/main/java/net/xfrage/goofyaahmod/GoofyAahModClient.java
@@ -0,0 +1,10 @@
+package net.xfrage.goofyaahmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GoofyAahModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
